1a95594db99fa3b7cd5ad8394f46dcdfccb8efdb
Trần Quang Huy <145090977+quanghuydsai@users.noreply.github.com>
2025-12-28 14:50:24 +0700

    Update Data Simulator/Producer

--- producer.py
+++ producer.py
@@ -5,15 +5,15 @@ producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda v: json.dumps(v).encode()
 )
-
+#List of simulated sensors
 sensor_ids = ['roadA','roadB','roadC']
 
 while True:
   data = {
-    'sensor': random.choice(sensor_ids),
-    'speed': random.randint(10, 60),
+    'sensor': random.choice(sensor_ids), #Choose a random sensor from the list of simulated sensors
+    'speed': random.randint(10, 60), #Generate a random speed between 10 and 60km/h
     'timestamp': int(time.time())
   }
   print("Sending", data)
   producer.send('traffic', data)
-  time.sleep(2)  # every 2 seconds
+  time.sleep(2) #Generate the event after every 2 seconds
